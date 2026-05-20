ec0096e038b1a3cd081679803c51f277b9348f62
milotruck <kohjishin@gmail.com>
2026-05-20 22:28:58 +0800

    bug fixes on implicit declaration,const pointer changes, Kp Kd changes

--- Common/moteus/moteus.h
+++ Common/moteus/moteus.h
@@ -32,14 +32,14 @@
 #include <stddef.h>
 
 /* STM32 HAL includes - user must include appropriate HAL header before this */
-#ifdef STM32G4xx_HAL_FDCAN_H
+#ifdef STM32H7xx_HAL_FDCAN_H
     /* Already included */
 #else
     /* Try to include it - will fail if HAL not configured */
-    #if defined(STM32G431xx) || defined(STM32G441xx) || defined(STM32G471xx) || \
+    #if defined(STM32H755xx) || defined(STM32G441xx) || defined(STM32G471xx) || \
         defined(STM32G473xx) || defined(STM32G474xx) || defined(STM32G483xx) || \
         defined(STM32G484xx) || defined(STM32G491xx) || defined(STM32G4A1xx)
-        #include "stm32g4xx_hal.h"
+        #include "stm32h7xx_hal.h"
     #endif
 #endif
 
